0430b2cc46f3302d534aa4a905f0c51dbb9dd7d5
xncbf <xncbf12@gmail.com>
2017-06-01 11:19:28 +0900

    created models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ImageInfo
+
 
 # Register your models here.
+class ImageInfoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'detail', 'origin_image', 'thumbnail')
+    search_fields = ('title', 'detail', 'origin_image', 'thumbnail')
+
+admin.site.register(ImageInfo, ImageInfoAdmin)
