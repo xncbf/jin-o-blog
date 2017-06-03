a12d0d0510f6be633d77dcd08f2f378e582c1334
xncbf <xncbf12@gmail.com>
2017-06-04 01:15:20 +0900

    d

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class ImageInfo(models.Model):
-    origin_image = models.ImageField('원본이미지', )
-    thumbnail = models.ImageField('썸네일', )
+    origin_image = models.ImageField('원본이미지', upload_to='images/fulls/')
+    thumbnail = models.ImageField('썸네일', upload_to='images/thumbs/')
     title = models.CharField('제목', max_length=500)
     detail = models.CharField('내용', max_length=500)
